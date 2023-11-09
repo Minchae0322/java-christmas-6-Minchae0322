942992c7eec674393d20de9298902d74c6a5136f
Minchae0322 <jmcabc0322@gmail.com>
2023-11-09 21:40:26 +0900

    feat: Menu 추상 클래스와 구현체 구현

---
+++ src/main/java/christmas/domain/menuImpl/Main.java
@@ -0,0 +1,9 @@
+package christmas.domain.menuImpl;
+
+import christmas.domain.Menu;
+
+public class Main extends Menu {
+    public Main(String menuName, Integer price) {
+        super(menuName, price);
+    }
+}
