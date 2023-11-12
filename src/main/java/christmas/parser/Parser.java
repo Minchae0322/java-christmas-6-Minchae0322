6fa175c1d32cf98232634f46c8b6f8955100c336
Minchae0322 <jmcabc0322@gmail.com>
2023-11-12 16:18:28 +0900

    refactor: parser 함수 분리

--- src/main/java/christmas/parser/Parser.java
+++ src/main/java/christmas/parser/Parser.java
@@ -12,20 +12,24 @@ public class Parser {
     public static HashMap<Menu, Integer> parseMenu(Restaurant restaurant, String menus) {
         String[] parsedMenu = menus.split(",");
         HashMap<Menu, Integer> menuInfo = new HashMap<>();
-        int amountSum = 0;
+
         for(String menu : parsedMenu) {
             String menuName = menu.split("-")[0];
-            validateOrderMenus(restaurant, menuName);
-            validateNumericAmount(menu.split("-")[1]);
             int amount = Integer.parseInt(menu.split("-")[1]);
-            amountSum += amount;
             menuInfo.put(restaurant.getEqualMenu(menuName), amount);
         }
+        return menuInfo;
+    }
+
+    public static void valid() {
+        /*int amountSum = 0;
+        validateOrderMenus(restaurant, menuName);
+        validateNumericAmount(menu.split("-")[1]);
+        amountSum += amount;
         if(amountSum < 0 || amountSum > 20) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
-        validateAllBeverage(menuInfo);
-        return menuInfo;
+        validateAllBeverage(menuInfo);*/
     }
 
 
--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -131,6 +131,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 주문_예외_테스트_형식() {
+        assertSimpleTest(() -> {
+            runException("3", "티본스테이크-15,,제로콜라-3");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
 
     @Override
     protected void runMain() {
